ebe91cf3f9f56649b9c3e9c2e4caca64a66d5ad0
Letícia <125403978+Lehguanaes@users.noreply.github.com>
2024-11-23 18:42:28 -0300

    Update visualizarDuvidas.php

--- glow_schedule/atendente/visualizarDuvidas.php
+++ glow_schedule/atendente/visualizarDuvidas.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dúvidas Recebidas</title>
+    <title>Care Tones</title>
+    <!-- Ícone para navegadores modernos -->
+    <link rel="icon" href="../logo/Logo.png" type="image/png">
+    <!-- Ícone para navegadores antigos -->
+    <link rel="shortcut icon" href="../logo/Logo.png" type="image/x-icon">
+    <!-- Links externos -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <!-- Estilização padrão do web site -->
     <link rel="stylesheet" href="../css/style.css">
     <!-- Estilização formulários de Perfis -->
@@ -16,39 +23,42 @@
     <link rel="stylesheet" href="../css/cards.css">
     <!-- Estilização Navbar -->
     <link rel="stylesheet" href="../css/navbar.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
 <body>
-<header>
+    <!-- Início da Navbar -->
+    <header>
         <nav class="nav-bar">
-        <a class="logo" href="Home.html"><img src="../logo/Logo.png" class="logoIMG">Care Tones</a>
-          <ul class="nav-list">
-            <li><a href="visualizarConsultas.php" class="nav">Agenda</a></li>
-            <li><a href="visualizarDuvidas.php" class="nav">Dúvidas</a></li>
-            <li><a href="../avaliacoes/avaliacoes.php" class="nav">Avaliações</a></li>
-            <li><a href="../agendamento/agendamentoAtendente.php" class="nav">Agendar Consulta</a></li>
-            <li><a href="../esteticista/esteticistas.php" class="nav">Esteticistas</a></li>
-          </ul>
-          <div class="dropdown">
-            <div class="login-icon">
-                <a href="perfilAtendente.php">
-                <i class="fa-solid fa-circle-user fa-xl" style="color: #fff;"></i>
-                </a>
-                <div class="dropdown-content">
-                    <a href="perfilAtendente.php"><i class="fa-solid fa-right-to-bracket" style="color: #cf6f7a;"></i> Perfil </a>
-                    <a href="../esteticista/cadastroEsteticistaAtendente.php"><i class="fa-solid fa-address-card" style="color: #cf6f7a;"></i> Cadastrar Esteticista </a>
-                    <a href="../cliente/cadastrarClienteAtendente.php"><i class="fa-solid fa-address-card" style="color: #cf6f7a;"></i> Cadastrar Cliente </a>
-                    <a href="../procedimento/cadastrarProcedimento.php"><i class="fa-solid fa-address-card" style="color: #cf6f7a;"></i> Cadastrar Procedimento </a>
-                </div>
+            <a class="logo" href="#"><img src="../logo/Logo.png" class="logoIMG">Care Tones</a>
+            <ul class="nav-list">
+                <li><a href="visualizarDuvidas.php" class="nav">Dúvidas</a></li>
+                <li><a href="visualizarAvaliacoes.php" class="nav">Avaliações</a></li>
+                <li><a href="../procedimento/consultarProcedimento.php" class="nav">Procedimentos</a></li>
+                <li><a href="visualizarConsultas.php" class="nav">Agenda</a></li>
+                <li><a href="../agendamentoAtendente/agendamento.php" class="nav">Agendamento</a></li>
+            </ul>
+            <div class="dropdown">
+                <div class="login-icon">
+                    <a href="perfilAtendente.php">
+                        <i class="fa-solid fa-circle-user fa-xl" style="color: #fff;"></i>
+                    </a>
+                    <div class="dropdown-content">
+                        <a href="perfilAtendente.php"><i class="fa-solid fa-user fa-sm" style="color: #cf6f7a;"></i> Perfil </a>
+                        <a href="../atendente/consultarCliente.php"><i class="fa-solid fa-users-line" style="color: #cf6f7a;"></i> Clientes </a>
+                        <a href="../atendente/consultarAtendente.php"><i class="fa-solid fa-user-tie" style="color: #cf6f7a;"></i> Atendentes</a>
+                        <a href="../atendente/consultarEsteticista.php"><i class="fa-solid fa-user-doctor" style="color: #cf6f7a;"></i> Profissionais </a>
+                        <a href="../procedimento/consultarProcedimento.php"><i class="fa-brands fa-shopify" style="color: #cf6f7a;"></i> Procedimentos </a>
+                        <a href="/glow_schedule/controller/logout.php"><i class="fa-solid fa-right-to-bracket fa-sm"></i> Sair</a>
+                    </div>
             </div>
-         </div>
-         <div class="mobile-menu">
-          <div class="line1"></div>
-          <div class="line2"></div>
-          <div class="line3"></div>
-        </div>
+            </div>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
         </nav>
-      </header>
+    </header>
+    <!-- Fim da Navbar -->
     <div id="Duvidas">
         <h2>Dúvidas Recebidas</h2>
         <!-- Mensagem de instruções do filtro -->
@@ -90,8 +100,6 @@
                         } else {
                             $data_envio_formatada = 'Sem data'; // Caso não haja data
                         }
-
-                
                         echo "
                             <div class='container'>
                                 <div class='card-simples'>
@@ -104,7 +112,6 @@
                                             <button onclick='lerDuvida({$duvida['id']})' class='btn-leitura' id='lerDuvida{$duvida['id']}'>Ler Dúvida</button>
                                             <div id='mensagemCompleta{$duvida['id']}' class='mensagem-expandida'>
                                                 <p>{$duvida['mensagem']}</p>";
-                                            
                                             // Verifica o status da dúvida para exibir o botão "Marcar como Respondida"
                                             if ($duvida['status'] === 'ativado') {
                                                 echo "<button onclick='marcarComoRespondida({$duvida['id']})' class='btn-respondida'>Marcar como Respondida</button>";
@@ -121,7 +128,76 @@
                 mysqli_close($conn);
             ?>
         </div>
-    </div>
+    </div><!-- Inicio Footer -->
+    <footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <a class="navbar-brand" href="#"> <img class="rounded-circle ms-4" src="../logo/Logo.png" alt="Logo care tones" width="69px"></a>
+                <h3>Care Tones</h3>  
+                <div id="footer_social_media">
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook-f fa-xs"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="whatsapp">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="localizacao">
+                        <i class="fa-solid fa-location-dot"></i>
+                    </a>
+                </div>
+            </div>
+            <ul class="footer-list">
+                <li>
+                    <h4 id="subtitulo-footer">Cadastros</h4>
+                </li>
+                <li>
+                    <a href="cadastrarClienteAtendente.php" class="footer-link">Cadastrar Cliente</a>
+                </li>
+                <li>
+                    <a href="cadastroAtendente.php" class="footer-link">Cadastrar Atendentes</a>
+                </li>
+                <li>
+                    <a href="cadastroEsteticista.php" class="footer-link">Cadastrar Profissionais</a>
+                </li>
+            </ul>
+            <ul class="footer-list">
+                <li>
+                    <h4 id="subtitulo-footer">Interesses</h4>
+                </li>
+                <li>
+                    <a href="visualizarConsultas.php" class="footer-link">Agenda</a>
+                </li>
+                <li>
+                    <a href="visualizarAvaliacoes.php" class="footer-link">Avaliações</a>
+                </li>
+                <li>
+                    <a href="visualizarDuvidas.php" class="footer-link">Dúvidas</a>
+                </li>
+            </ul>
+            <div id="footer_subscribe">
+                <h4 id="subtitulo-footer">Clínica</h4>
+                <p>
+                    Venha visualizar o que temos!
+                </p>
+                <ul class="footer-list">
+                <li>
+                    <a href="../esteticista/esteticistas.php" class="footer-link">Profissionais</a>
+                </li>
+                <li>
+                    <a href="../procedimento/procedimentos.php" class="footer-link">Procedimentos</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+        <div id="footer_copyright">
+            &#169
+            2024 all rights reserved
+        </div>
+    </footer>
+    <!-- Link Sweet Alert -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- Link Js Navbar -->
     <script src="../js/navbar.js"></script>

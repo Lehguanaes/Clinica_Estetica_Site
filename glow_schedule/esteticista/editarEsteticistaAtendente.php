38815e9e9b239dbf6e7bef5691f7818cf2747edc
Nico <125403659+n1ico@users.noreply.github.com>
2024-11-24 01:45:10 -0300

    Add files via upload

--- glow_schedule/esteticista/editarEsteticistaAtendente.php
+++ glow_schedule/esteticista/editarEsteticistaAtendente.php
@@ -47,45 +47,56 @@ if (isset($_GET['token_esteticista'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Perfil Esteticista</title>
+    <title>Editar Esteticistas</title>
+    <!-- Ícone para navegadores modernos -->
+    <link rel="icon" href="../logo/Logo.png" type="image/png">
+    <!-- Ícone para navegadores antigos -->
+    <link rel="shortcut icon" href="../logo/Logo.png" type="image/x-icon">
+    <!-- Links externos -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Estilização padrão do web site -->
     <link rel="stylesheet" href="../css/style.css">
+    <!-- Estilização formulários de Perfis -->
     <link rel="stylesheet" href="../css/perfil.css">
+    <!-- Estilização Navbar -->
+    <link rel="stylesheet" href="../css/navbar.css">
 </head>
 <body>
-<header>
+    <!-- Início da Navbar -->
+    <header>
         <nav class="nav-bar">
-          <a class="logo" href="Home.html"><img src="../logo/Logo.png" class="logoIMG">Care Tones</a>
-          <ul class="nav-list">
-            <li><a href="./atendente/visualizarConsultas.php" class="nav">Agenda</a></li>
-            <li><a href="../atendente/visualizarDuvidas.php" class="nav">Dúvidas</a></li>
-            <li><a href="../avaliacoes/avaliacoes.php" class="nav">Avaliações</a></li>
-            <li><a href="../agendamento/agendamentoAtendente.php" class="nav">Agendar Consulta</a></li>
-            <li><a href="esteticistas.php" class="nav">Esteticistas</a></li>
-          </ul>
-          <div class="dropdown">
-            <div class="login-icon">
-                <a href="../atendente/perfilAtendente.php">
-                <i class="fa-solid fa-circle-user fa-xl" style="color: #fff;"></i>
-                </a>
-                <div class="dropdown-content">
-                    <a href="perfilAtendente.php"><i class="fa-solid fa-right-to-bracket" style="color: #cf6f7a;"></i> Perfil </a>
-                    <a href="cadastroEsteticistaAtendente.php"><i class="fa-solid fa-address-card" style="color: #cf6f7a;"></i> Cadastrar Esteticista </a>
-                    <a href="../cliente/cadastrarClienteAtendente.php"><i class="fa-solid fa-address-card" style="color: #cf6f7a;"></i> Cadastrar Cliente </a>
-                    <a href="../procedimento/cadastrarProcedimento.php"><i class="fa-solid fa-address-card" style="color: #cf6f7a;"></i> Cadastrar Procedimento </a>
-                </div>
+            <a class="logo" href="#"><img src="../logo/Logo.png" class="logoIMG">Care Tones</a>
+            <ul class="nav-list">
+                <li><a href="visualizarDuvidas.php" class="nav">Dúvidas</a></li>
+                <li><a href="visualizarAvaliacoes.php" class="nav">Avaliações</a></li>
+                <li><a href="../procedimento/consultarProcedimento.php" class="nav">Procedimentos</a></li>
+                <li><a href="visualizarConsultas.php" class="nav">Agenda</a></li>
+                <li><a href="../agendamento/agendamentoAtendente.php" class="nav">Agendamento</a></li>
+            </ul>
+            <div class="dropdown">
+                <div class="login-icon">
+                    <a href="perfilAtendente.php">
+                        <i class="fa-solid fa-circle-user fa-xl" style="color: #fff;"></i>
+                    </a>
+                    <div class="dropdown-content">
+                        <a href="perfilAtendente.php"><i class="fa-solid fa-user fa-sm" style="color: #cf6f7a;"></i> Perfil </a>
+                        <a href="../atendente/consultarCliente.php"><i class="fa-solid fa-users-line" style="color: #cf6f7a;"></i> Clientes </a>
+                        <a href="../atendente/consultarAtendente.php"><i class="fa-solid fa-user-tie" style="color: #cf6f7a;"></i> Atendentes</a>
+                        <a href="../procedimento/consultarProcedimento.php"><i class="fa-brands fa-shopify" style="color: #cf6f7a;"></i> Procedimentos </a>
+                        <a href="/glow_schedule/controller/logout.php"><i class="fa-solid fa-right-to-bracket fa-sm"></i> Sair</a>
+                        <a href="../atendente/consultarEsteticista.php"><i class="fa-solid fa-user-doctor" style="color: #cf6f7a;"></i> Voltar  </a>
+                    </div>
             </div>
-         </div>
-         <div class="mobile-menu">
-          <div class="line1"></div>
-          <div class="line2"></div>
-          <div class="line3"></div>
-        </div>
+            </div>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
         </nav>
-      </header>
+    </header>
     <h2>Editar Perfil</h2>
     <section class="container">
         <form method="POST" action="../controller\esteticista\esteticistaController.php" enctype="multipart/form-data" class="form" id="form_perfil">

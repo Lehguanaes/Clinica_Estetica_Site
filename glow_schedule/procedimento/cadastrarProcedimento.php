182cd34ffe69a684b090f345ea7e605e74d0e5ce
Letícia <125403978+Lehguanaes@users.noreply.github.com>
2024-11-25 01:15:16 -0300

    Update cadastrarProcedimento.php

--- glow_schedule/procedimento/cadastrarProcedimento.php
+++ glow_schedule/procedimento/cadastrarProcedimento.php
@@ -3,70 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Procedimento</title>
+    <title>Care Tones</title>
+    <!-- Ícone para navegadores modernos -->
+    <link rel="icon" href="../logo/Logo.png" type="image/png">
+    <!-- Ícone para navegadores antigos -->
+    <link rel="shortcut icon" href="../logo/Logo.png" type="image/x-icon">
+    <!-- Links externos -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Estilização formulários de Perfis -->
-    <link rel="stylesheet" href="../css/perfil.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <!-- Estilização padrão do web site -->
     <link rel="stylesheet" href="../css/style.css">
-  <!-- Estilização Navbar -->
-  <link rel="stylesheet" href="/glow_schedule/css/navbar.css">
+    <!-- Estilização formulários de Perfis -->
+    <link rel="stylesheet" href="../css/perfil.css">
+    <!-- Estilização Navbar -->
+    <link rel="stylesheet" href="../css/navbar.css">
 </head>
 <body>
- <!-- Início da Navbar -->
- <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand"> <img class="rounded-circle ms-4" src="../logo/Logo.png" alt="Logo care tones" width="69px"> </a>
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Cinzel&family=Playfair+Display:ital@1&display=swap" rel="stylesheet">
-            <div class="logo">
-                <a class="nav-link" id="desativado" aria-current="page" href="home.php">Care Tones</a>
-            </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent" >
-                <ul class="navbar-nav w-auto">
-                    <li class="nav-item pe-4 ps-4">
-                        <a class="nav-link" aria-current="page" href="/glow_schedule/cliente/home.php">Home</a>
-                    </li>
-                    <li class="nav-item pe-4 ps-4">
-                    <a class="nav-link active" aria-current="page" href="/glow_schedule/cliente/sobreNos.php">Sobre Nós</a>
-                    </li>
-                    <li class="nav-item pe-4 ps-4">
-                        <a class="nav-link active" aria-current="page" href="/glow_schedule/procedimento/procedimentos.php">Procedimentos</a>
-                    </li>
-                    <li class="nav-item pe-4 ps-4">
-                    <li class="nav-item pe-4 ps-4">
-                        <a class="nav-link active" aria-current="page" href="/glow_schedule/esteticista/esteticistas.php"> Nossos Profissionais</a>
-                   </li>
-                    <li class="nav-item pe-4 ps-4">
-                        <a class="nav-link active" aria-current="page" href="/glow_schedule/agendamento/agendamento.php">Agendamentos</a>
-                    </li>
-                </ul>
-                <div class="dropdown">
-                    <div class="login-icon">
-                        <a href="Cadastro.html">
+    <!-- Início da Navbar -->
+    <header>
+        <nav class="nav-bar">
+            <a class="logo" href="#"><img src="../logo/Logo.png" class="logoIMG">Care Tones</a>
+            <ul class="nav-list">
+                <li><a href="../atendente/visualizarDuvidas.php" class="nav">Dúvidas</a></li>
+                <li><a href="../atendente/visualizarAvaliacoes.php" class="nav">Avaliações</a></li>
+                <li><a href="../procedimento/consultarProcedimento.php" class="nav">Procedimentos</a></li>
+                <li><a href="visualizarConsultas.php" class="nav">Agenda</a></li>
+                <li><a href="../agendamentoAtendente/agendamento.php" class="nav">Agendamento</a></li>
+            </ul>
+            <div class="dropdown">
+                <div class="login-icon">
+                    <a href="perfilAtendente.php">
                         <i class="fa-solid fa-circle-user fa-xl" style="color: #fff;"></i>
-                        </a>
-                        <div class="dropdown-content">
-                            <a href="perfilCliente.php"><i class="fa-solid fa-pen-to-square"></i> Meu perfil</a>
-                            <a href="consultasCliente.php"><i class="fa-solid fa-calendar-days"></i> Minhas Consultas</a>
-                            <a href="avaliacoes.php"><i class="fa-solid fa-ranking-star"></i> Avaliar</a>
-                            <a href="/glow_schedule/controller/logout.php"><i class="fa-solid fa-right-to-bracket"></i> Sair</a>
-                        </div>
+                    </a>
+                    <div class="dropdown-content">
+                        <a href="../atendente/perfilAtendente.php"><i class="fa-solid fa-user fa-sm" style="color: #cf6f7a;"></i> Perfil </a>
+                        <a href="../atendente/consultarCliente.php"><i class="fa-solid fa-users-line" style="color: #cf6f7a;"></i> Clientes </a>
+                        <a href="../atendente/consultarAtendente.php"><i class="fa-solid fa-user-tie" style="color: #cf6f7a;"></i> Atendentes</a>
+                        <a href="../atendente/consultarEsteticista.php"><i class="fa-solid fa-user-doctor" style="color: #cf6f7a;"></i> Profissionais </a>
+                        <a href="../procedimento/consultarProcedimento.php"><i class="fa-brands fa-shopify" style="color: #cf6f7a;"></i> Procedimentos </a>
+                        <a href="/glow_schedule/controller/logout.php"><i class="fa-solid fa-right-to-bracket fa-sm"></i> Sair</a>
                     </div>
-                </div>
             </div>
-        </div>
-    </nav>
+            </div> 
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </nav>
+    </header>
     <!-- Fim da Navbar -->
     <h2>Cadastro de Procedimento</h2>
-
     <!-- Cadastro de Procedimento -->
     <section class="container">
         <form method="POST" action="/glow_schedule/controller/esteticista/esteticistaController.php" enctype="multipart/form-data" class="form" id="form_perfil">
@@ -96,7 +85,6 @@
                     <input type="text" class="form-control" id="preco_procedimento" name="preco_procedimento" placeholder="Digite o preço do procedimento" required>
                 </div>
             </div>
-
             <!-- Duração do Procedimento -->
             <div class="column">
                 <div class="input-box">
@@ -127,7 +115,77 @@
             <button id="botao_cadastrar" type="submit" class="btn btn-primary">Cadastrar</button>
         </form>
     </section>
-
+    <!-- Inicio Footer -->
+    <footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <a class="navbar-brand" href="#"> <img class="rounded-circle ms-4" src="../logo/Logo.png" alt="Logo care tones" width="69px"></a>
+                <h3>Care Tones</h3>  
+                <div id="footer_social_media">
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook-f fa-xs"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="whatsapp">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="localizacao">
+                        <i class="fa-solid fa-location-dot"></i>
+                    </a>
+                </div>
+            </div>
+            <ul class="footer-list">
+                <li>
+                    <h4 id="subtitulo-footer">Cadastros</h4>
+                </li>
+                <li>
+                    <a href="../atendente/cadastrarClienteAtendente.php" class="footer-link">Cadastrar Cliente</a>
+                </li>
+                <li>
+                    <a href="../atendente/cadastroAtendente.php" class="footer-link">Cadastrar Atendentes</a>
+                </li>
+                <li>
+                    <a href="../atendente/cadastroEsteticista.php" class="footer-link">Cadastrar Profissionais</a>
+                </li>
+            </ul>
+            <ul class="footer-list">
+                <li>
+                    <h4 id="subtitulo-footer">Interesses</h4>
+                </li>
+                <li>
+                    <a href="../atendente/visualizarConsultas.php" class="footer-link">Agenda</a>
+                </li>
+                <li>
+                    <a href="../atendente/visualizarAvaliacoes.php" class="footer-link">Avaliações</a>
+                </li>
+                <li>
+                    <a href="../atendente/visualizarDuvidas.php" class="footer-link">Dúvidas</a>
+                </li>
+            </ul>
+            <div id="footer_subscribe">
+                <h4 id="subtitulo-footer">Clínica</h4>
+                <p>
+                    Venha visualizar o que temos!
+                </p>
+                <ul class="footer-list">
+                <li>
+                    <a href="../esteticista/esteticistas.php" class="footer-link">Profissionais</a>
+                </li>
+                <li>
+                    <a href="../procedimento/procedimentos.php" class="footer-link">Procedimentos</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+        <div id="footer_copyright">
+            &#169
+            2024 all rights reserved
+        </div>
+    </footer>
+    <!-- Link Js Navbar -->
+    <script src="../js/navbar.js"></script>
     <!-- Script de Pré-visualização da Imagem -->
     <script>
         function previewProcedimentoPic() {

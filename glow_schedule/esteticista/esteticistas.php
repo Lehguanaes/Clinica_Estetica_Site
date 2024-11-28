79a2e50a12ed1354903f5e0ec88b2539a8c7f7e2
emycspri <127248691+emycspri@users.noreply.github.com>
2024-11-27 22:49:54 -0300

    atualizando navbar e footer

--- glow_schedule/esteticista/esteticistas.php
+++ glow_schedule/esteticista/esteticistas.php
@@ -28,33 +28,28 @@
         <!-- Início da Navbar -->
         <header>
         <nav class="nav-bar">
-          <a class="logo" href="Home.html"><img src="../logo/Logo.png" class="logoIMG">Care Tones</a>
-          <ul class="nav-list">
-            <li><a href="../atendente/visualizarConsultas.php" class="nav">Consultas</a></li>
-            <li><a href="../atendente/visualizarDuvidas.php" class="nav">Dúvidas</a></li>
-            <li><a href="../avaliacoes/avaliacoes.php" class="nav">Avaliações</a></li>
-            <li><a href="../agendamento/agendamentoAtendente.php" class="nav">Agendar Consulta</a></li>
-            <li><a href="esteticistas.php" class="nav">Esteticistas</a></li>
-            <li><a href="../atendente/cadastroAtendente.php" class="nav">Novos Atendentes</a></li>
-          </ul>
-          <div class="dropdown">
-            <div class="login-icon">
-                <a href="perfilAtendente.php">
-                <i class="fa-solid fa-circle-user fa-xl" style="color: #fff;"></i>
-                </a>
-                <div class="dropdown-content">
-                    <a href="../atendente/perfilAtendente.php"><i class="fa-solid fa-right-to-bracket" style="color: #cf6f7a;"></i> Perfil </a>
-                    <a href="cadastroEsteticista.php"><i class="fa-solid fa-address-card" style="color: #cf6f7a;"></i> Cadastrar Esteticista </a>
-                    <a href="../cliente/cadastrarClienteAtendente.php"><i class="fa-solid fa-address-card" style="color: #cf6f7a;"></i> Cadastrar Cliente </a>
-                    <a href="../procedimento/cadastrarProcedimento.php"><i class="fa-solid fa-address-card" style="color: #cf6f7a;"></i> Cadastrar Procedimento </a>
-                </div>
+            <a class="logo" href="#"><img src="../logo/Logo.png" class="logoIMG">Care Tones</a>
+            <ul class="nav-list">
+                <li><a href="esteticistas.php" class="nav">Profissionais</a>
+                <li><a href="../procedimento/procedimentos.php" class="nav">Procedimentos</a>
+                <li><a href="visualizarConsultas.php" class="nav">Agenda</a></li>
+            </ul>
+            <div class="dropdown">
+                <div class="login-icon">
+                    <a href="perfilAtendente.php">
+                        <i class="fa-solid fa-circle-user fa-xl" style="color: #fff;"></i>
+                    </a>
+                    <div class="dropdown-content">
+                        <a href="perfilEsteticista.php"><i class="fa-solid fa-user fa-sm" style="color: #cf6f7a;"></i> Perfil </a>
+                        <a href="/glow_schedule/controller/logout.php"><i class="fa-solid fa-right-to-bracket fa-sm"></i> Sair</a>
+                    </div>
             </div>
-         </div>
-         <div class="mobile-menu">
-          <div class="line1"></div>
-          <div class="line2"></div>
-          <div class="line3"></div>
-        </div>
+            </div>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
         </nav>
       </header>
     <!-- Fim da Navbar -->
@@ -108,41 +103,66 @@
         <button type="submit"><a href="cadastroEsteticista.php">Cadastrar Esteticistas</a></button>
     </div>
 </div>
-    <footer>
+<footer>
         <div id="footer_content">
             <div id="footer_contacts">
-                <a class="navbar-brand"><img class="rounded-circle ms-4" src="../logo/Logo.png" alt="Logo Care Tones" width="69px"></a>
-                <h3>Care Tones</h3>
+                <a class="navbar-brand" href="#"> <img class="rounded-circle ms-4" src="../logo/Logo.png" alt="Logo care tones" width="69px"></a>
+                <h3>Care Tones</h3>  
                 <div id="footer_social_media">
-                    <a href="#" class="footer-link" id="instagram"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="#" class="footer-link" id="facebook"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="#" class="footer-link" id="whatsapp"><i class="fa-brands fa-whatsapp"></i></a>
-                    <a href="#" class="footer-link" id="localizacao"><i class="fa-solid fa-location-dot"></i></a>
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook-f fa-xs"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="whatsapp">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                    <a href="#" class="footer-link" id="localizacao">
+                        <i class="fa-solid fa-location-dot"></i>
+                    </a>
                 </div>
             </div>
             <ul class="footer-list">
-                <li><h4 id="subtitulo-footer">Quem somos?</h4></li>
-                <li><a href="#" class="footer-link">Sobre nós</a></li>
-                <li><a href="#" class="footer-link">Clínica</a></li>
-                <li><a href="#" class="footer-link">Profissionais</a></li>
+                <li>
+                    <h4 id="subtitulo-footer">Cadastros</h4>
+                </li>
+                <li>
+                    <a href="perfilEsteticista.php" class="footer-link">Perfil</a>
+                </li>
+                <li>
+                    <a href="cadastroAtendente.php" class="footer-link">Editar Perfil</a>
+                </li>
             </ul>
             <ul class="footer-list">
-                <li><h4 id="subtitulo-footer">Interesses</h4></li>
-                <li><a href="#" class="footer-link">Curiosidades</a></li>
-                <li><a href="#" class="footer-link">Promoções</a></li>
-                <li><a href="#" class="footer-link">Avaliações</a></li>
+                <li>
+                    <h4 id="subtitulo-footer">Interesses</h4>
+                </li>
+                <li>
+                    <a href="visualizarConsultas.php" class="footer-link">Agenda</a>
+                </li>
+                <li>
+                    <a href="../agendamentoAtendente/agendamento.php" class="footer-link">Agendamento</a>
+                </li>
             </ul>
             <div id="footer_subscribe">
-                <h4 id="subtitulo-footer">Cadastre-se</h4>
-                <p>Cadastre-se, venha conhecer nosso trabalho e saber das novidades!</p>
-                <div id="input_group">
-                    <input type="email" id="email">
-                    <button><i class="fa-regular fa-envelope"></i></button>
-                </div>
+                <h4 id="subtitulo-footer">Clínica</h4>
+                <p>
+                    Venha visualizar o que temos!
+                </p>
+                <ul class="footer-list">
+                <li>
+                    <a href="../esteticista/esteticistas.php" class="footer-link">Profissionais</a>
+                </li>
+                <li>
+                    <a href="../procedimento/procedimentos.php" class="footer-link">Procedimentos</a>
+                </li>
+                </ul>
             </div>
         </div>
         <div id="footer_copyright">
-            &#169 2023 all rights reserved
+            &#169
+            2024 all rights reserved
         </div>
     </footer>
 </body>
